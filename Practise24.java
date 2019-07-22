663833334bfcfdf66409ac471ea4b98d116a90cb
Wweakness <770199484@qq.com>
2019-07-22 14:52:53 +0800

    二分查找

---
+++ Practise24.java
@@ -0,0 +1,29 @@
+//二分查找
+public class Practise24{
+	public static int binarySearch(int[] array,int key){
+        int left=0;
+        int right=array.length-1;
+        int mid=(left+right)/2;
+		while(left<=right){
+		if(key<array[mid]){
+			right=mid-1;
+		}
+		else if(key>array[mid]){
+		    left=mid+1;
+		}
+		else {
+		    key=array[mid];
+			return mid;
+		}
+		}
+		return -1;
+	}
+	
+	
+	public static void main(String[] args){
+		int[] array={2,3,4,5,6,7,8};
+		int c=binarySearch(array,3);
+		System.out.printf("3的坐标是%d",c);
+	}
+}
+	
